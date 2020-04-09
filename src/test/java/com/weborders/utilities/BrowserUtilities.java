42872e1b6ebf7c9d435a37905da836b4641a6816
Jarylgapova <Karima.jarylgapova@gmail.com>
2020-04-09 13:56:43 -0500

    added utilitty for switching windows

--- src/test/java/com/weborders/utilities/BrowserUtilities.java
+++ src/test/java/com/weborders/utilities/BrowserUtilities.java
@@ -112,18 +112,15 @@ public class BrowserUtilities {
         return path;
     }
 
-    /**
-     * This method will switch webdriver from current window
-     * to target window based on page title
-     * @param title of the window to switch
-     */
-    public static void scitchWindow(String title){
-        Set<String> windowHandles = Driver.getDriver().getWindowHandles();
-        for(String window : windowHandles){
-            Driver.getDriver().switchTo().window(window);
-            if(Driver.getDriver().getTitle().equals(title)){
-                break;
-            }
-        }
-    }
+   public static void switchWindow(String title){
+
+       Set<String> windowHandles = Driver.getDriver().getWindowHandles();
+       for (String window : windowHandles) {
+           Driver.getDriver().switchTo().window(window);
+
+           if (Driver.getDriver().getTitle().equals(title)) {
+               break;
+           }
+       }
+   }
 }
